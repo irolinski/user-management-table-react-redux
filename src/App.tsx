9ed9d66b2dc98a9209b9766230d7d3c8cb9b8536
szczek <ignacy.rolinski@gmail.com>
2024-09-08 01:22:03 +0200

    fix(search): ignore spaces while searching phone numbers

--- src/App.tsx
+++ src/App.tsx
@@ -61,13 +61,8 @@ const App = () => {
       u.email.toLowerCase().includes(searchState.email.toLowerCase()) &&
       u.phone
         .toLowerCase()
-        //omit hyphens from search
-        .replace(/-/g, "")
-        .includes(
-          searchState.phone
-            //omit hyphens from search
-            .replace(/-/g, "")
-        )
+        .replace(/[- .]/g, "")
+        .includes(searchState.phone.replace(/[- .]/g, ""))
   );
 
   // pagination
@@ -80,133 +75,129 @@ const App = () => {
   }
 
   return (
-    <div>
-      <div className="container">
-        <h1 className="title text-3xl text-center">User Management Table</h1>
-        <main>
-          {getUsersRes.isLoading ? (
-            <span className="loader"></span>
-          ) : (
-            <section className="table-menu-wrapper">
-              <menu className="h-24 my-4 flex justify-between w-full">
-                <div className="h-full flex flex-col justify-center">
-                  <label htmlFor="results-per-page">Display: </label>
-                  <ResultsPerPage value={paginateTable.resultsPerPage} />
-                </div>
-                <div className="h-full flex">
-                  <div className="p-4 h-full ">
-                    <div id="search-button">
-                      <button onClick={() => dispatch(toggleSearch())}>
-                        <img className="w-8 m-1" src={SearchIcon} />
-                      </button>
-                    </div>
-                    <label htmlFor="search-button">Search</label>
-                  </div>
-                  <div className="p-3 h-full">
-                    <div id="standardize-switch">
-                      <StandardizeSwitch
-                        isActive={standardizeIsActive}
-                        handleToggle={() => dispatch(toggleStandardize())}
-                        colorOne={"#FFFF"}
-                        colorTwo={"black"}
-                      />
-                    </div>
-                    <label htmlFor="standardize-switch">Standardize data</label>
-                  </div>
-                </div>
-              </menu>
-              <div className="table-wrapper">
-                <table className="user-table relative">
-                  <thead>
-                    <tr>
-                      <th className="name-col">Name</th>
-                      <th className="username-col">Username</th>
-                      <th className="email-col">E-mail</th>
-                      <th className="phone-col ">Phone</th>
-                    </tr>
-                    <tr
-                      className={`search-row overflow-hidden ${
-                        showSearch ? "hidden" : ""
-                      } `}
-                    >
-                      <th className="name-col">
-                        <SearchInput
-                          value={searchState.name}
-                          searchFunction={searchByName}
-                          displayedPage={paginateTable.displayedPage}
-                        />
-                      </th>
-                      <th className="username-col">
-                        <SearchInput
-                          value={searchState.username}
-                          searchFunction={searchByUsername}
-                          displayedPage={paginateTable.displayedPage}
-                        />
-                      </th>
-                      <th className="email-col">
-                        <SearchInput
-                          value={searchState.email}
-                          searchFunction={searchByEmail}
-                          displayedPage={paginateTable.displayedPage}
-                        />
-                      </th>
-                      <th className="phone-col">
-                        <SearchInput
-                          value={searchState.phone}
-                          searchFunction={searchByPhone}
-                          displayedPage={paginateTable.displayedPage}
-                        />
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="">
-                    {users.length > 0 ? (
-                      users.map((u: UserInterface, index: number) => (
-                        <tr
-                          className={`${
-                            index <
-                              paginateTable.displayedPage *
-                                paginateTable.resultsPerPage &&
-                            index >=
-                              (paginateTable.displayedPage - 1) *
-                                paginateTable.resultsPerPage
-                              ? "table-row"
-                              : "hidden"
-                          }`}
-                          key={index}
-                        >
-                          <td className="name-col">{u.name}</td>
-                          <td className="username-col">{u.username}</td>
-                          <td className="email-col">{u.email}</td>
-                          <td className="phone-col whitespace-break-spaces">
-                            {u.phone}
-                          </td>
-                        </tr>
-                      ))
-                    ) : (
-                      <TableBodyPlaceholder
-                        message={"No results to display."}
-                      />
-                    )}
-                  </tbody>
-                </table>
+    <div className="container">
+      <h1 className="title text-3xl text-center">User Management Table</h1>
+      <main>
+        {getUsersRes.isLoading ? (
+          <span className="loader"></span>
+        ) : (
+          <section className="table-menu-wrapper">
+            <menu className="h-24 my-4 flex justify-between w-full">
+              <div className="h-full flex flex-col justify-center">
+                <label htmlFor="results-per-page">Display: </label>
+                <ResultsPerPage value={paginateTable.resultsPerPage} />
               </div>
-              <section className="table-footer w-full p-4">
-                <div className="w-full flex justify-between">
-                  <AmountInfo
-                    allResultsNum={users.length}
-                    perPageResultsNum={paginateTable.resultsPerPage}
-                  />
-                  <PaginationButtons
-                    pagesArr={pagesArr}
-                    displayedPage={paginateTable.displayedPage}
-                  />
+              <div className="h-full flex">
+                <div className="p-4 h-full ">
+                  <div id="search-button">
+                    <button onClick={() => dispatch(toggleSearch())}>
+                      <img className="w-8 m-1" src={SearchIcon} />
+                    </button>
+                  </div>
+                  <label htmlFor="search-button">Search</label>
                 </div>
-              </section>
+                <div className="p-3 h-full">
+                  <div id="standardize-switch">
+                    <StandardizeSwitch
+                      isActive={standardizeIsActive}
+                      handleToggle={() => dispatch(toggleStandardize())}
+                      colorOne={"#FFFF"}
+                      colorTwo={"black"}
+                    />
+                  </div>
+                  <label htmlFor="standardize-switch">Standardize data</label>
+                </div>
+              </div>
+            </menu>
+            <div className="table-wrapper">
+              <table className="user-table relative">
+                <thead>
+                  <tr>
+                    <th className="name-col">Name</th>
+                    <th className="username-col">Username</th>
+                    <th className="email-col">E-mail</th>
+                    <th className="phone-col ">Phone</th>
+                  </tr>
+                  <tr
+                    className={`search-row overflow-hidden ${
+                      showSearch ? "hidden" : ""
+                    } `}
+                  >
+                    <th className="name-col">
+                      <SearchInput
+                        value={searchState.name}
+                        searchFunction={searchByName}
+                        displayedPage={paginateTable.displayedPage}
+                      />
+                    </th>
+                    <th className="username-col">
+                      <SearchInput
+                        value={searchState.username}
+                        searchFunction={searchByUsername}
+                        displayedPage={paginateTable.displayedPage}
+                      />
+                    </th>
+                    <th className="email-col">
+                      <SearchInput
+                        value={searchState.email}
+                        searchFunction={searchByEmail}
+                        displayedPage={paginateTable.displayedPage}
+                      />
+                    </th>
+                    <th className="phone-col">
+                      <SearchInput
+                        value={searchState.phone}
+                        searchFunction={searchByPhone}
+                        displayedPage={paginateTable.displayedPage}
+                      />
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="">
+                  {users.length > 0 ? (
+                    users.map((u: UserInterface, index: number) => (
+                      <tr
+                        className={`${
+                          index <
+                            paginateTable.displayedPage *
+                              paginateTable.resultsPerPage &&
+                          index >=
+                            (paginateTable.displayedPage - 1) *
+                              paginateTable.resultsPerPage
+                            ? "table-row"
+                            : "hidden"
+                        }`}
+                        key={index}
+                      >
+                        <td className="name-col">{u.name}</td>
+                        <td className="username-col">{u.username}</td>
+                        <td className="email-col">{u.email}</td>
+                        <td className="phone-col whitespace-break-spaces">
+                          {u.phone}
+                        </td>
+                      </tr>
+                    ))
+                  ) : (
+                    <TableBodyPlaceholder message={"No results to display."} />
+                  )}
+                </tbody>
+              </table>
+            </div>
+            <section className="table-footer w-full p-4">
+              <div className="w-full flex justify-between">
+                <AmountInfo
+                  allResultsNum={users.length}
+                  perPageResultsNum={paginateTable.resultsPerPage}
+                />
+                <PaginationButtons
+                  pagesArr={pagesArr}
+                  displayedPage={paginateTable.displayedPage}
+                />
+              </div>
             </section>
-          )}
-        </main>
-      </div>
+          </section>
+        )}
+      </main>
     </div>
   );
 };
